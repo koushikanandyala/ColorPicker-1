a43c369ca9b410920ac10de00725b064ec7c6e82
koushikanandyala <koushika1729@gmail.com>
2024-11-08 14:24:40 +0530

    first commitnew.css

---
+++ script.js
@@ -0,0 +1,29 @@
+let containerEl = document.getElementById("colorPickercont");
+let spanEl = document.getElementById("selectedcolor")
+
+
+//let buton1 = document.getElementById("btn1");
+//let buton2 = document.getElementById("btn2");
+//let buton3 = document.getElementById("btn3");
+//let buton4 = document.getElementById("btn4");
+
+function button1(){
+    containerEl.style.backgroundColor="#e0e0e0";
+    spanEl.textContent = "#e0e0e0";
+    
+}
+function button2(){
+    containerEl.style.backgroundColor="#6fcf97";
+    spanEl.textContent = "#6fcf97";
+    
+}
+function button3(){
+    containerEl.style.backgroundColor="#56ccf2";
+    spanEl.textContent = "#56ccf2";
+    
+}
+function button4(){
+    containerEl.style.backgroundColor="#bb6bd9";
+    spanEl.textContent = "#bb6bd9";
+    
+}
